f1ceee78bcb1cc0dc164e7605632b5cc0f075eb0
Gennaro <gennarinodi02@gmail.com>
2022-03-25 17:58:32 +0100

    pausa

---
+++ jsnack7/main7.js
@@ -0,0 +1,5 @@
+stampaCubo (n)
+{
+ n
+}
+
---
+++ jsnack8/main8.js
@@ -0,0 +1,25 @@
+
+const array =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 5 ];
+
+let insieme = sommanumeri();
+console.log(risultato);
+
+
+function sommanumeri ( arrey2, somma)
+ {
+    const array2 =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 5 ];
+
+    
+    for (let i = 0; i < array.length; i++) 
+    {
+       
+  
+        somma = somma+ array[i];
+        
+    }
+    return somma ;
+   
+  
+ }
+
+
