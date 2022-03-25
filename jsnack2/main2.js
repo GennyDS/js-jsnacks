a419faa8fe2b38bdaf5c6b580550e2d1ff48a937
Gennaro <gennarinodi02@gmail.com>
2022-03-25 17:01:02 +0100

    finito

--- jsnack2/main2.js
+++ jsnack2/main2.js
@@ -1,10 +1,28 @@
-let parola1 = prompt("inserisci una PAROLA");
-let parola2 = prompt("inserisci un altra PAROLA");
+let a = prompt("inserisci una parola")
+let b = prompt ("inserisci un altra parola");
+let cont1 = 0;
+let cont2 = 0;
 
+for(let i = 0; i<=a.length; i++)
+{
+  cont1++;
+}
+for(let i = 0; i<=b.length; i++)
+{
+  cont2++;
+}
 
-lettera
-
-
-
-
-
+if(cont1>cont2) 
+{
+    console.log( a + " è maggiore di " + b)
+}
+else if(cont1<cont2)
+  {
+    console.log( b +" è maggiore di " + a)
+  }
+  else
+  {
+      console.log("le parole hanno la stessa lunghezza :  " +a +"---" +b)
+  }
+    
+    
