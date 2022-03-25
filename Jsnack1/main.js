a6bf97f9aeb5426dc2294b7812a7ac5ed2f662c5
Gennaro <gennarinodi02@gmail.com>
2022-03-25 14:14:27 +0100

    FINITO ES 1

---
+++ Jsnack1/main.js
@@ -0,0 +1,17 @@
+let n1 = prompt("inserisci un numero");
+let n2 = prompt("inserisci un altro numero");
+
+if(n1>n2)
+{
+  console.log("il numero più grande è :" + n1 );
+ 
+}
+else if(n1<n2)
+{
+    console.log("il numero più grande è :" + n2 );
+}
+else
+{
+    console.log("i numeri sono uguali");
+}
+
