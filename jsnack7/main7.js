ceaa6e178760996e936fff5cb4e8648320735739
Gennaro <gennarinodi02@gmail.com>
2022-03-28 10:33:25 +0200

    finito

--- jsnack7/main7.js
+++ jsnack7/main7.js
@@ -1,5 +1,11 @@
-stampaCubo (n)
+ function stampaCubo(n)
 {
- n
+ for(let i=0;i<n;i++)
+ {
+ cubo=i**3;
+ console.log(cubo);
+ }
 }
 
+let nNumero=prompt("inserisci un numero")
+stampaCubo(nNumero);
